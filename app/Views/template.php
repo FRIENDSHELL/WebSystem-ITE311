301430b3da20448496d4485531ccc5b1c2a4d938
FRIENDSHELL <friendshellegaran41@gmail.com>
2025-08-07 19:26:50 +0800

    Added Bootstrap template and Home controller

--- app/Views/template.php
+++ app/Views/template.php
@@ -2,25 +2,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>My Bootstrap CI4 App</title>
+    <title>My CodeIgniter System</title>
 
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
+<head>
+    <style>
+        .navbar-pink {
+            background-color: pink;
+        }
+    </style>
 </head>
 <body>
 
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-pink">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">My CodeIgniter</a>
+            <a class="navbar-brand" href="#">NavBar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav"> 
                     <li class="nav-item">
                         <a class="nav-link active" href="#">Home</a>
                     </li>
